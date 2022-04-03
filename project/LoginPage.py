213b8bbe58e61251ae706105b972f8f4fca67b05
reshihaibucuo <102245433+reshihaibucuo@users.noreply.github.com>
2022-04-03 17:05:13 +0800

    Add files via upload
    Finishing pages, 'Sign in', 'Sign up', and 'Forget Password'

--- project/LoginPage.py
+++ project/LoginPage.py
@@ -1,11 +1,38 @@
-from pyhtml import html
-from LoginPage import dataset
+from email import message
+from pyhtml import p
+# from LoginPage import dataset
 
-def LoginPage():
-    response = html(
-        # TODO put your pyhtml code here
-    )
-    pass
+def LoginPage(error=False):
+    if not error:
+        with open(".\login.html", "r") as fl:
+            response = fl.read()
+    else:
+        with open(".\login.html", "r") as fl:
+            contents = fl.readlines()
+        contents.insert(25, """<p style="color:red; font-size: 14px;">The email and/or password you specified are not correct.</p>""")
+        response = ''.join(contents)
+    return response
 
-def RegistrationPage():
-    pass
+def RegistrationPage(error=False):
+    if not error:
+        with open(".\RegistrationPage.html", "r") as fl:
+         response = fl.read()
+    else:
+        with open(".\RegistrationPage.html", "r") as fl:
+            contents = fl.readlines()
+        message = "E-mail address has been used, please try again."
+        contents.insert(34, f"""<p style="color:red; font-size: 14px;">{message}</p>""")
+        response = ''.join(contents)
+    return response
+    
+def PasswordReset():
+    with open(".\PasswordReset.html", "r") as fl:
+         response = fl.read()
+    return response
+
+def user_login(email, password):
+    return True
+def email_repeat(email):
+    return True
+def check_email(email):
+    return True
--- project/server.py
+++ project/server.py
@@ -1,24 +1,32 @@
-from flask import Flask, request
+# import imp
+from flask import Flask, redirect, request, url_for
 from pyhtml import html # TODO add imports
-
+import LoginPage 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '08ccc5b937fa253dfb0462d9289aa05d7ae5d3ada0ce1876f5743b1f3b88bd3e'
+app.config['SECRET_KEY'] = '08ccccb937fa253dfb0462d9289aa05d7ae5d3ada0ce1876f5743b1f3b88bd3e'
 
 @app.route('/', methods=["GET", "POST"])
-def homepage():
-    if request.method == 'POST':
-        # TODO you received form data, handle it
-        # (usually produce HTML)
-        pass
-    else:
-        # TODO you didn't receive form data
-        # (usually produce HTML)
-        pass
-        
-    response = html(
-        # TODO put your pyhtml code here
-    )
-    return response
+def homepage():    
+    return redirect(url_for('resetpassword'))
 
+@app.route('/login', methods=["GET", "POST"])
+def login():
+    login_error = False
+    if 'password' in request.form and 'email' in request.form:
+        login_error = LoginPage.user_login(request.form['email'], request.form['password'])
+        # TODO: load user profile form json file into session.
+    return LoginPage.LoginPage(login_error)
+    
+@app.route('/signup', methods=["GET", "POST"])   
+def signup():
+    sign_uperror=False
+    if 'email' in request.form:
+        sign_uperror = LoginPage.email_repeat(request.form['email'])
+
+    return LoginPage.RegistrationPage(sign_uperror)
+
+@app.route('/resetpassword', methods=["GET", "POST"])
+def resetpassword():
+    return LoginPage.PasswordReset()
 if __name__ == "__main__":
     app.run(debug=True)
